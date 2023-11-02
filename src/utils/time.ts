0fc69b2be31d49c6c651cbd796bf907487618eee
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-11-02 23:59:31 +0900

    refactor: createdAt 파싱 함수 예외처리 로직 추가

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,4 +1,6 @@
 export const parseCreatedAt = (createdAt: string) => {
+  if (!createdAt) return { date: null, time: null };
+
   const [date, timeWithMilliseconds] = createdAt.split('T');
   const time = timeWithMilliseconds.split('.').toString().slice(0, 5);
 
