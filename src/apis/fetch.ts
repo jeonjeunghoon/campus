2f186c73223ae6727d9cac6a6fde986b115a2432
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-11-03 14:20:39 +0900

    feat: mutation 함수 구현

--- src/apis/fetch.ts
+++ src/apis/fetch.ts
@@ -13,5 +13,25 @@ export const fetchApis = () => {
     }
   };
 
-  return { get };
+  const mutation = async <RequestBody>(
+    method: 'POST' | 'PATCH' | 'PUT' | 'DELETE',
+    requestUrl: string,
+    body: RequestBody | null = null,
+  ) => {
+    try {
+      const response = await fetch(requestUrl, {
+        method,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      });
+
+      if (!response.ok) throw new Error('Network response was not ok ' + response.statusText);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return { get, mutation };
 };
