7bea6651ba13b7ff6891da755215c9a1b2005734
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-11-08 17:17:22 +0900

    feat: Editor Page 이동 링크 컴포넌트 추가

--- src/pages/AnnouncementDashboard/index.tsx
+++ src/pages/AnnouncementDashboard/index.tsx
@@ -1,5 +1,36 @@
+import { Link } from 'react-router-dom';
+
+import styled from '@emotion/styled';
+
 import List from 'components/Announcement/List';
+import { ROUTES } from 'constants/routes';
 
 export default function AnnouncementDashboard() {
-  return <List isDashboard={true} />;
+  return (
+    <div>
+      <List isDashboard={true} />
+      <S.AddLink to={ROUTES.announcementEditor.path}>+</S.AddLink>
+    </div>
+  );
 }
+
+const S = {
+  AddLink: styled(Link)`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    bottom: 28px;
+    right: 28px;
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+
+    background-color: ${({ theme }) => theme.colors.primary};
+    font-size: 5.2rem;
+    &:active,
+    &:visited {
+      color: white;
+    }
+  `,
+};
