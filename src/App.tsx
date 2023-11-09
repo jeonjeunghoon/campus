ccf327f224a08498070675fcf0c341abcd3fc859
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-11-09 16:08:52 +0900

    refactor: 동작하지 않는 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,20 @@
-import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQueryErrorResetBoundary,
-} from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import styled from '@emotion/styled';
 
-import Error from 'pages/Error';
-
-import ErrorBoundary from 'components/ErrorBoundary';
 import Header from 'components/Header';
 
 const queryClient = new QueryClient();
 
 export default function App() {
-  const { reset } = useQueryErrorResetBoundary();
-
   return (
     <QueryClientProvider client={queryClient}>
-      <Suspense fallback={<div>서스펜스</div>}>
-        <ErrorBoundary onReset={reset} fallback={Error}>
-          <S.Container>
-            <Header />
-            <Outlet />
-          </S.Container>
-        </ErrorBoundary>
-      </Suspense>
+      <S.Container>
+        <Header />
+        <Outlet />
+      </S.Container>
     </QueryClientProvider>
   );
 }
