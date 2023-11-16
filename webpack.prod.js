3749d0c4fa9fec0656ff492417f6b8434c214fd3
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-11-16 15:36:36 +0900

    chore: webpack prod 설정 수정

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,11 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'hidden-source-map',
+  plugins: [new Dotenv({ path: './.env.production' })],
   optimization: {
     minimizer: ['...'],
   },
