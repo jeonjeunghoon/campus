0ad06f53167aa3af856b00ea193f0a49a816ecc8
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-12-23 22:07:50 +0900

    feat: 전역 에러바운더리, 서스펜스 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
+import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import styled from '@emotion/styled';
 
+import ErrorBoundary from 'components/ErrorBoundary';
 import Header from 'components/Header';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <S.Container>
-        <Header />
-        <Outlet />
-      </S.Container>
+      <ErrorBoundary fallback={<div>전역 에러 바운더리</div>}>
+        <Suspense fallback={<div>전역 서스펜스</div>}>
+          <S.Container>
+            <Header />
+            <Outlet />
+          </S.Container>
+        </Suspense>
+      </ErrorBoundary>
     </QueryClientProvider>
   );
 }
