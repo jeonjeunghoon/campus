77aab07b40f43fe8087c063bdb0ea17703ceb679
jeonjeunghoon <jeonjeunghoon@gmail.com>
2024-01-04 16:21:55 +0900

    refactor: `QueryClient` 위치 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
 import styled from '@emotion/styled';
 
 import ErrorBoundary from 'components/ErrorBoundary';
 import Header from 'components/Header';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: false,
-    },
-  },
-});
-
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ErrorBoundary fallback={<div>전역 에러 바운더리</div>}>
-        <Suspense fallback={<div>전역 서스펜스</div>}>
-          <S.Layout>
-            <Header />
-            <S.Content>
-              <Outlet />
-            </S.Content>
-          </S.Layout>
-        </Suspense>
-      </ErrorBoundary>
-    </QueryClientProvider>
+    <ErrorBoundary fallback={<div>전역 에러 바운더리</div>}>
+      <Suspense fallback={<div>전역 서스펜스</div>}>
+        <S.Layout>
+          <Header />
+          <S.Content>
+            <Outlet />
+          </S.Content>
+        </S.Layout>
+      </Suspense>
+    </ErrorBoundary>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client';
 
 import { Global, ThemeProvider } from '@emotion/react';
@@ -13,11 +14,21 @@ import Router from 'pages/Router';
 
 await deferRender();
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <Global styles={globalStyle} />
-      <Router />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <Global styles={globalStyle} />
+        <Router />
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
