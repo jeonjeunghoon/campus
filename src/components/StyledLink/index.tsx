06ac4b413f23dff7b30c36615f703b7864ad7cad
jeonjeunghoon <jeonjeunghoon@gmail.com>
2024-01-04 15:48:23 +0900

    refactor: `StyledLink` 사이즈 속성 제거

--- src/components/StyledLink/index.tsx
+++ src/components/StyledLink/index.tsx
@@ -2,33 +2,30 @@ import { LinkProps, Link } from 'react-router-dom';
 
 import styled from '@emotion/styled';
 
-import { ColorList, SizeList, VariantList } from 'type/style';
+import { ColorList, VariantList } from 'type/style';
 
 type Props = {
   to: string;
   variant?: VariantList;
-  size?: SizeList;
   color?: ColorList;
 } & LinkProps;
 
 export default function StyledLink({
   to,
   variant = 'primary',
-  size = 'medium',
   color = 'primary',
   children,
   ...rest
 }: Props) {
   return (
-    <S.StyledLink to={to} variant={variant} size={size} color={color} {...rest}>
+    <S.StyledLink to={to} variant={variant} color={color} {...rest}>
       {children}
     </S.StyledLink>
   );
 }
 
 const S = {
-  StyledLink: styled(Link)<{ variant: VariantList; size: SizeList; color: ColorList }>`
-    ${({ theme, size }) => theme.size.button[size]}
+  StyledLink: styled(Link)<{ variant: VariantList; color: ColorList }>`
     background-color: ${({ theme, variant }) => theme.colors.button[variant]};
     color: ${({ theme, color }) => theme.colors.button.text[color]};
 
